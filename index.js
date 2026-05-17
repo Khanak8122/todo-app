6a76db205e35cb4d5814dd17e867e1d595286362
Khanak Gupta <khanakgupta2908@gmail.com>
2026-05-17 16:58:03 +0530

    feat: initial todo API with 4 endpoints

---
+++ index.js
@@ -0,0 +1,46 @@
+const express=require('express');
+const app=express();
+app.use(express.json());
+
+let todos=[];
+let nextId=1;
+
+//Get all todos
+app.get('/todos',(req,res)=>{
+    res.json(todos);
+});
+
+//POST-Create a new todo
+app.post('/todos',(req,res)=>{
+    const {title}=req.body;
+
+    if(!title)
+    {
+        return res.status(400).json({error:'Title is required'});
+    }
+
+    const todo={id:nextId++,title,done:false};
+    todos.push(todo);
+    res.status(201).json(todo);
+});
+
+//PUT-Mark a  todo as done/undone
+app.put('/todos/:id', (req,res)=>{
+     const todo=todos.find(t=>t.id===parseInt(req.params.id));
+     if(!todo) return todo.status(404).json({error:'Todo not found'});
+
+     todo.done=!todo.done;
+     res.json(todo);
+});
+
+//DELETE-Delete a todo
+app.delete('/todos/:id',(req,res)=>{
+   const index=todos.findIndex(t=>t.id===parseInt(req.params.id));
+   if(index===-1) return res.status(404).json({error:'Todo not found'});
+   todos.splice(index,1);
+   res.status(204).send();
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000');
+});
