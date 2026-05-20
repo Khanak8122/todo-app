9d637466a268177c1c20bce5f8f391c2b96590c5
Khanak Gupta <khanakgupta2908@gmail.com>
2026-05-20 11:13:23 +0530

    ci: add github actions pipeline with playwright api tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    timeout: 10000,
+    timeout: 15000,
     use: {
         baseURL: 'http://localhost:3000',
     },
+    reporter:[
+        ['list'],
+        ['html' , {open: 'never' }],
+    ],
 });
